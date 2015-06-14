12dc4da921fbdeed558c222804a09df009fe21cb
Luciano Restifo <luciano.restifo@gmail.com>
2015-06-14 22:58:46 +0200

    Removed debugging code

--- rt_tpm_tickets.sh
+++ rt_tpm_tickets.sh
@@ -5,8 +5,8 @@
 # Author:   Thu Dec 18 21:54:06 CET 2014 Luciano Restifo [lrestifo@esselte.com]
 # Description:
 #   Creates tickets in RT by taking ticket content from one or more text files
-#   named after today - i.e. if you run this script on Tuesdays, it will first
-#   look for files named 'tuesday_ticket*.txt', and for each one of them it will
+#   named after today - i.e. if you run this script on Tuesdays, it will look
+#   for files named 'tuesday_ticket*.txt', and for each one of them it will
 #   create an RT ticket whose content is taken by reading the file.  Input files
 #   are simple text files whose content is passed to the RT REST API unchanged.
 # Purpose:
@@ -36,5 +36,5 @@ tktfiles="/usr/local/etc/`date '+%A' | tr '[:upper:]' '[:lower:]'`_ticket"
 sed 's/ /=/' ~/.rtrc >/tmp/rtrc && source /tmp/rtrc && rm /tmp/rtrc
 for tkt in $tktfiles*.txt
 do
-  [ -s $tkt ] && echo /usr/local/bin/http --check-status --ignore-stdin --body --form POST $server/REST/1.0/ticket/new user==$user pass==$passwd content=@$tkt
+  [ -s $tkt ] && /usr/local/bin/http --check-status --ignore-stdin --body --form POST $server/REST/1.0/ticket/new user==$user pass==$passwd content=@$tkt
 done
