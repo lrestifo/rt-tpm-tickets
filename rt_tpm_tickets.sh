c58eb8b4d9c003958e856caf4776fc64e72efe20
Luciano Restifo <luciano.restifo@gmail.com>
2015-06-14 22:22:19 +0200

    Added support for multiple files (creation of tickets also for Uelzen)

--- rt_tpm_tickets.sh
+++ rt_tpm_tickets.sh
@@ -4,17 +4,21 @@
 # Title:    RT_TPM_TICKETS -- Create the RT ticket of the day
 # Author:   Thu Dec 18 21:54:06 CET 2014 Luciano Restifo [lrestifo@esselte.com]
 # Description:
-#   Creates a ticket in RT by taking ticket content from a text file named
-#   after today - i.e. if you run this script on Tuesdays, it will create an RT
-#   ticket whose content is taken from a file named "tuesday_ticket.txt"
+#   Creates tickets in RT by taking ticket content from one or more text files
+#   named after today - i.e. if you run this script on Tuesdays, it will first
+#   look for files named 'tuesday_ticket*.txt', and for each one of them it will
+#   create an RT ticket whose content is taken by reading the file.  Input files
+#   are simple text files whose content is passed to the RT REST API unchanged.
 # Purpose:
 #   Automated creation of TPM Task tickets like changing a backup tape
 # Configuration:
-#   basic authentication credentials are taken from ~/.netrc
-#   RT server URL and App credentials are read from ~/.rtrc
+#   1. Tickets text files must be located in /usr/local/etc
+#   2. Authentication credentials are taken from ~/.netrc
+#   3. RT server URL and App credentials are read from ~/.rtrc
 # Notes:
+#   File names must be lower case
 #   This script is meant to be run by cron
-#   Uses httpie to communicate over the RT REST interface
+#   It uses httpie to communicate over the RT REST interface
 #   Outputs http status and ticket number
 # References:
 #   cron(1), netrc(5), rt(1), http --help
@@ -28,7 +32,9 @@ function die() {
 
 [ -s ~/.rtrc  ] || die "Can't open ~/.rtrc"
 [ -s ~/.netrc ] || die "Can't open ~/.netrc"
-tkt="/usr/local/etc/`date '+%A' | tr '[:upper:]' '[:lower:]'`_ticket.txt"
-[ -s $tkt ] || die "No $tkt, nothing to do."
+tktfiles="/usr/local/etc/`date '+%A' | tr '[:upper:]' '[:lower:]'`_ticket"
 sed 's/ /=/' ~/.rtrc >/tmp/rtrc && source /tmp/rtrc && rm /tmp/rtrc
-http --check-status --ignore-stdin --body --form POST $server/REST/1.0/ticket/new user==$user pass==$passwd content=@$tkt
+for tkt in $tktfiles*.txt
+do
+  echo http --check-status --ignore-stdin --body --form POST $server/REST/1.0/ticket/new user==$user pass==$passwd content=@$tkt
+done
